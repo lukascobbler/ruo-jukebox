53c495175194b74119558112cb280d0a42eb28b7
Momir Stanišić <momir.stanisic.ns@gmail.com>
2025-10-20 01:39:59 +0200

    fix: share shared_layer with git

--- back-end/app.py
+++ back-end/app.py
@@ -1,18 +1,17 @@
-from iac.auth_layer_stack import AuthLayerStack
 from iac.domain_stacks.subscriptions_stack import SubscriptionsStack
 from iac.domain_stacks.interactions_stack import InteractionsStack
 from iac.domain_stacks.playlists_stack import PlaylistsStack
 from iac.domain_stacks.artists_stack import ArtistsStack
-# from iac.domain_stacks.users_stack import UsersStack
 from iac.domain_stacks.albums_stack import AlbumsStack
 from iac.domain_stacks.genres_stack import GenresStack
 from iac.domain_stacks.songs_stack import SongsStack
 from iac.shared_layer_stack import SharedLayerStack
 from iac.domain_stacks.auth_stack import AuthStack
 from iac.api_gateway_stack import ApiGatewayStack
+from iac.auth_layer_stack import AuthLayerStack
 from iac.dynamo_db_stack import DynamoDbStack
 from iac.cognito_stack import CognitoStack
-from aws_cdk import App, Environment, Fn
+from aws_cdk import App, Environment
 from iac.s3_stack import S3Stack
 
 app = App()
