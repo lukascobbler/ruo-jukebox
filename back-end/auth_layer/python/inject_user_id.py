c6c42761e3072285a4ec6929956debfe43f81501
Luka <79410671+lukascobbler@users.noreply.github.com>
2025-10-20 01:03:52 +0200

    misc: adding user role to every lambda event

--- back-end/auth_layer/python/inject_user_id.py
+++ back-end/auth_layer/python/inject_user_id.py
@@ -8,5 +8,13 @@ def inject_user_id(event: dict):
         raise AuthorizationException("No claims found in requestContext.authorizer")
 
     event["userId"] = claims.get("sub")
+    user_group = claims.get("cognito:groups")
+    if not user_group:
+        raise AuthorizationException("User has no assigned group")
+
+    if isinstance(user_group, list):
+        user_group = user_group[0]
+
+    event["userRole"] = user_group
 
     return claims
