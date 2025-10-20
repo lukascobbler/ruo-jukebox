8fe0dd05d5070ca57f8dba5f4bb3419888dabe99
Momir Stanišić <momir.stanisic.ns@gmail.com>
2025-10-20 03:51:29 +0200

    hotfix: iac

--- back-end/iac/domain_stacks/artists_stack.py
+++ back-end/iac/domain_stacks/artists_stack.py
@@ -1,8 +1,8 @@
-from iac.auth_layer_stack import AuthLayerStack
 from iac.constructs.lambda_with_permissions import LambdaWithPermissions
 from aws_cdk import (Stack, aws_apigateway as apigw)
 from iac.shared_layer_stack import SharedLayerStack
 from iac.api_gateway_stack import ApiGatewayStack
+from iac.auth_layer_stack import AuthLayerStack
 from iac.dynamo_db_stack import DynamoDbStack
 from iac.s3_stack import S3Stack
 from constructs import Construct
@@ -10,11 +10,12 @@ from constructs import Construct
 
 class ArtistsStack(Stack):
     def __init__(self, scope: Construct, id: str,
-                 dynamo_db: DynamoDbStack, s3: S3Stack, shared_layer_stack: SharedLayerStack, auth_layer_stack: AuthLayerStack,
+                 dynamo_db: DynamoDbStack, s3: S3Stack, shared_layer_stack: SharedLayerStack, auth_layer_stack: AuthLayerStack, api_stack: ApiGatewayStack,
                  environment, **kwargs):
         super().__init__(scope, id, **kwargs)
         self.lambdas = {}
         self._create_lambdas(dynamo_db, s3, shared_layer_stack, auth_layer_stack, environment)
+        self._attach_to_api(api_stack)
 
     def _create_lambdas(self, dynamo_db, s3, shared_layer_stack, auth_layer_stack, env):
         lambda_defs = {
@@ -29,7 +30,7 @@ class ArtistsStack(Stack):
         for key, path in lambda_defs.items():
             self.lambdas[key] = LambdaWithPermissions(self, key, path, env, dynamo_db, s3, shared_layer_stack, auth_layer_stack).fn
 
-    def attach_to_api(self, api_gateway: ApiGatewayStack):
+    def _attach_to_api(self, api_gateway: ApiGatewayStack):
         artists = api_gateway.api.root.add_resource("artists")
         artist_id = artists.add_resource("{id}")
 
