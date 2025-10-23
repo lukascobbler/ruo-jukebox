0ae0a68853d8e77cde1f138033db8d9893035ade
Momir Stanišić <momir.stanisic.ns@gmail.com>
2025-10-23 21:02:35 +0200

    merge: subscriptions for albums

--- back-end/iac/domains/content/albums_stack.py
+++ back-end/iac/domains/content/albums_stack.py
@@ -6,24 +6,22 @@ from iac.layers.libs_layer_stack import LibsLayerStack
 from iac.layers.auth_layer_stack import AuthLayerStack
 from iac.shared.dynamo_db_stack import DynamoDbStack
 from iac.shared.s3_stack import S3Stack
-from constructs import Construct
 from aws_cdk import aws_sqs as sqs
+from constructs import Construct
+
 
 class AlbumsStack(NestedStack):
     def __init__(self, scope: Construct, stack_id: str,
                  dynamo_db: DynamoDbStack, s3: S3Stack, libs_layer_stack: LibsLayerStack, auth_layer_stack: AuthLayerStack,
-                 utils_layer_stack: UtilsLayerStack, api_stack: ApiGatewayStack, environment, notify_queue: sqs.IQueue, **kwargs):
+                 utils_layer_stack: UtilsLayerStack, api_stack: ApiGatewayStack, environment, branch: str, notify_queue: sqs.IQueue, **kwargs):
         super().__init__(scope, stack_id, **kwargs)
         self.lambdas = {}
         environment = {**environment, "NEW_CONTENT_QUEUE_URL": notify_queue.queue_url}
-        self._create_lambdas(dynamo_db, s3, libs_layer_stack, auth_layer_stack, utils_layer_stack, environment)
+        self._create_lambdas(dynamo_db, s3, libs_layer_stack, auth_layer_stack, utils_layer_stack, environment, branch)
         self._attach_to_api(api_stack)
         notify_queue.grant_send_messages(self.lambdas["AlbumsCompleteUpload"])
 
-
-
-
-    def _create_lambdas(self, dynamo_db, s3, libs_layer_stack, auth_layer_stack, utils_layer_stack, env):
+    def _create_lambdas(self, dynamo_db, s3, libs_layer_stack, auth_layer_stack, utils_layer_stack, env, branch):
         lambda_defs = {
             "AlbumsList": "services/albums/list",
             "AlbumsGet": "services/albums/get",
@@ -33,7 +31,7 @@ class AlbumsStack(NestedStack):
             "AlbumsCompleteUpload": "services/albums/complete_upload"
         }
         for key, path in lambda_defs.items():
-            self.lambdas[key] = LambdaWithPermissions(self, key, path, env, dynamo_db, s3, libs_layer_stack, auth_layer_stack, utils_layer_stack).fn
+            self.lambdas[key] = LambdaWithPermissions(self, key, path, env, dynamo_db, s3, libs_layer_stack, auth_layer_stack, utils_layer_stack, branch).fn
 
     def _attach_to_api(self, api: ApiGatewayStack):
         albums = api.api.root.add_resource("album")
